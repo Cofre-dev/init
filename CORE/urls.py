d74cafc3fc53cbb2c3cf80a74bea5ec7f5261c2d
Matias <rojascofrem@gmail.com>
2025-05-05 12:05:38 -0400

    test 2
    ups, olvide los otros archivos too

--- CORE/urls.py
+++ CORE/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.obtener_indicadores, name='main_page'),  # La ruta vacía ahora apunta a tu vista
+    path('', views.obtener_indicadores, name='main_page'),
+    path('descargar-dolar/', views.descargar_excel_dolar, name='descargar_excel_dolar'),
 ]
